3ab3054225a13422e1b3f7fbf448e106c886d9d1
Odarell35 <leratomawash0@gmail.com>
2024-01-13 10:22:11 +0200

    tasks 2

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	var selectedAmenities = [];
+	$('input[type="checkbox"]').change(function() {
+		var amenityId = $(this).attr('id');
+		if ($(this).prop('checked')) {
+			selectedAmenities.push(amenityId);
+		}		
+		else {
+			selectedAmenities = selectedAmenities.filter(function(id) {
+				return id !== amenityId;
+				});
+}
+$('.popover h4').text(selectedAmenities.join(', '));
+});
+});
+
+$('document').ready(function () {
+  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
+  $.get(url, function (res) {
+    if (res.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
