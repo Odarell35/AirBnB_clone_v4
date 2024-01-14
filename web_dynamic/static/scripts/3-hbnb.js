89e67dd3d2f73bcfe4ea0a85452575aae5668c6d
Odarell35 <leratomawash0@gmail.com>
2024-01-14 11:40:31 +0200

    tak 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,27 +1,48 @@
-/*javascript for the Scritps*/
 document.addEventListener('DOMContentLoaded', function () {
-	fetch('http://0.0.0.0:5001/api/v1/places_search/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}),
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Loop through the result and create article tags for each place
-        data.forEach(place => {
-            const placeArticle = document.createElement('article');
-            placeArticle.innerHTML = `
-                <div>
-                    <h2>${place.name}</h2>
-                    <p>${place.description}</p>
-                    <!-- Additional place information as needed -->
-                </div>
-            `;
-            document.querySelector('.places').appendChild(placeArticle);
-        });
-    })
-    .catch(error => console.error('Error:', error));
-});
+    const apiUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+    const sectionPlaces = document.querySelector('section.places');
 
+    // Function to create an article tag representing a Place
+    function createPlaceArticle(place) {
+        const article = document.createElement('article');
+        article.innerHTML = `
+		<div class="title_box">
+          		<h2>${place.name}</h2>
+          		<div class="price_by_night">$${place.price_by_night}</div>
+        	</div>
+        	<div class="information">
+          	<div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+          	<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+          	<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+        	</div>
+        	<div class="description">
+          	${place.description}
+        	</div> 
+	`;
+        return article;
+    }
+
+    // Function to make the API request and create articles
+    function getPlacesAndCreateArticles() {
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({}),
+        })
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(place => {
+                    const article = createPlaceArticle(place);
+                    sectionPlaces.appendChild(article);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching places:', error);
+            });
+    }
+
+    getPlacesAndCreateArticles();
+});
+	
